c14250d0cedd4584bd3ab54ef6c71571d7c0426d
009Abhaykumar <abhayanju58@gmail.com>
2026-03-31 09:32:43 +0530

    All Products Screens Done

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ import { name as appName } from './app.json';
 import FAQScreen from './src/screens/auth/OtpVerify';
 import ProfilePage from './src/screens/patient/PatientDetails';
 import ProductsScreen from './src/screens/profile/ProfilePage';
+import  ReviewPage from './src/'
 
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ReviewPage);
 
 
